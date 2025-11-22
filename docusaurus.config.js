c075bd9d2e3ff0ead7e1f9131efedb43e110c281
MingMimi96 <mingmimi96@gmail.com>
2025-11-22 12:29:03 +0800

    Configure for Github Pages deployment - Step 15

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://github.com/MingMimi96/my-maker-blog-Mimi.git',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'my-maker-blog-Mimi/',
+  baseUrl: '/my-maker-blog-Mimi/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
